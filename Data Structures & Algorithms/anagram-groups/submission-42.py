6260bbb4fe50b5624c289e428c5a0e28c002b500
dereksun64 <dereksun64@gmail.com>
2026-07-26 00:37:16 +0800

    Add: anagram-groups - submission-42

---
+++ Data Structures & Algorithms/anagram-groups/submission-42.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        lookup = defaultdict(list)
+
+        for str in strs:
+            counter = [0]*26
+            for c in str:
+                counter[ord(c) - ord("a")] += 1
+            lookup[tuple(counter)].append(str)
+        
+        return list(lookup.values())
