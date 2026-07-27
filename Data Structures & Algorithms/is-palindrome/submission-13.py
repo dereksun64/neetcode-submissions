8b68310550918dcd07d7f40d69cbbf9609945fbe
dereksun64 <dereksun64@gmail.com>
2026-07-27 17:05:09 +0800

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = []
+        for c in s:
+            if c.isalnum(): a.append(c)
+        
+        t = "".join(a)
+
+        l = 0 
+        r = len(t)-1
+
+        while l<r:
+            if t[l].lower() != t[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
